57d4c9470a29acabf377e47fd1938a3af9943f73
steliomo <steliomo@gmail.com>
2018-11-07 01:42:10 +0200

    CON-09: Create Exam type and the endpoint resource

---
+++ consultation-core/src/main/java/mz/co/msaude/consultation/core/examtype/dao/ExamTypeDAO.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+package mz.co.msaude.consultation.core.examtype.dao;
+
+import mz.co.msaude.boot.frameworks.dao.GenericDAO;
+import mz.co.msaude.consultation.core.examtype.model.ExamType;
+
+/**
+ * @author Stélio Moiane
+ *
+ */
+public interface ExamTypeDAO extends GenericDAO<ExamType, Long> {
+
+}
